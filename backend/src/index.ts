ac6d4c21a493be1c50ec796544ce887659862425
Edward <46017382+echang49@users.noreply.github.com>
2022-11-15 17:10:10 -0500

    Setup backend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
