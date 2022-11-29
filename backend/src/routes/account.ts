bc6c6ef3216a09cc3a184717fe794243c81ec6fd
Edward <46017382+echang49@users.noreply.github.com>
2022-11-28 19:34:58 -0500

    Add endpoints skeleton
    Add each endpoint to be implemented with comments and no logic

--- backend/src/routes/account.ts
+++ backend/src/routes/account.ts
@@ -1,5 +1,38 @@
 import express, { Request, Response } from 'express'
+import Web3Utils from 'web3-utils'
+
+import { Accounts } from '../models/account'
 
 export module Account {
     export const router = express.Router();
+
+    /**
+     * PURPOSE: Returns public information relating to a wallet's public address
+     * USAGE: Returns nonce to be signed and verified
+     */
+    router.get('/publicAddress', async (req: Request, res: Response) => {
+        if(typeof req.query.key !== 'string' || Web3Utils.isAddress(req.query.key) === false)
+            return res.sendStatus(404);
+        
+        const existingAccount = await Accounts.findOne({publicAddress: req.query.key}, 'nonce').lean();
+        if(existingAccount) {
+            return res.send({
+                nonce: existingAccount.nonce
+            });
+        }
+
+        const newAccount = new Accounts({ publicAddress: req.query.key });
+        await newAccount.save();
+        return res.send({
+            nonce: newAccount.nonce
+        });
+    });
+
+    /**
+     * PURPOSE: Returns a user's list of NFTs if the request comes from the requested user
+     * USAGE: Allow the user to choose which NFT to add as a rental
+     */
+    router.get('/nfts', (req: Request, res: Response) => {
+        
+    });
 }
--- backend/src/routes/nft.ts
+++ backend/src/routes/nft.ts
@@ -2,4 +2,51 @@ import express, { Request, Response } from 'express'
 
 export module NFT {
     export const router = express.Router();
+
+    /**
+     * PURPOSE: Returns basic information of every NFT rental listing
+     * USAGE: Display NFT rentals onto the user interface
+     * TODO: Add pagination for infinite scrolling by returning an ID to start querying from.
+     */
+    router.get('/listings', async (req: Request, res: Response) => {
+        
+    });
+
+    /**
+     * PURPOSE: Returns detailed information on a single NFT rental listing
+     * USAGE: Display NFT rental onto the user interface
+     */
+    router.get('/listing', async (req: Request, res: Response) => {
+        
+    });
+
+    /**
+     * PURPOSE: Creates a single NFT rental listing
+     * TODO: Go into the blockchain and clear all future rentals
+     */
+    router.post('/listing', async (req: Request, res: Response) => {
+        
+    });
+    
+    /**
+     * PURPOSE: Edits a single NFT rental listing
+     */
+    router.put('/listing', async (req: Request, res: Response) => {
+
+    });
+
+    /**
+     * PURPOSE: Deletes a single NFT rental listing
+     * TODO: If using an internal wallet as a third party escrow, return the NFT. Wait until rental ends if rented.
+     */
+    router.delete('/listing', async (req: Request, res: Response) => {
+        
+    });
+
+    /**
+     * PURPOSE: Create a rental object for a corresponding NFT rental listing and accept payment
+     */
+    router.post('rent', async (req: Request, res: Response) => {
+        
+    });
 }
