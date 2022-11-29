5563ee61a389e7f136db6fd14dc301b636e4983d
Edward <46017382+echang49@users.noreply.github.com>
2022-11-28 22:47:03 -0500

    Get listing info
    backend endpoints to get basic information on all listings and detailed information on a single listing

--- backend/src/routes/nft.ts
+++ backend/src/routes/nft.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response } from 'express'
 
+import { verifySignature } from '../config/authenticate'
+import { Listings } from '../models/listing'
+
 export module NFT {
     export const router = express.Router();
 
     /**
      * PURPOSE: Returns basic information of every NFT rental listing
-     * USAGE: Display NFT rentals onto the user interface
+     * USAGE: Display available NFT rentals onto the user interface
      * TODO: Add pagination for infinite scrolling by returning an ID to start querying from.
      */
     router.get('/listings', async (req: Request, res: Response) => {
-        
+        const { available } = req.query;
+        const listings = await Listings.find({available: available}, 'image name').lean();
+        return res.send(listings);
     });
 
     /**
@@ -17,21 +22,23 @@ export module NFT {
      * USAGE: Display NFT rental onto the user interface
      */
     router.get('/listing', async (req: Request, res: Response) => {
-        
+        const { id } = req.query;
+        const listing = await Listings.findOne({_id: id}).lean();
+        return res.send(listing);
     });
 
     /**
      * PURPOSE: Creates a single NFT rental listing
      * TODO: Go into the blockchain and clear all future rentals
      */
-    router.post('/listing', async (req: Request, res: Response) => {
+    router.post('/listing', verifySignature, async (req: Request, res: Response) => {
         
     });
     
     /**
      * PURPOSE: Edits a single NFT rental listing
      */
-    router.put('/listing', async (req: Request, res: Response) => {
+    router.put('/listing', verifySignature, async (req: Request, res: Response) => {
 
     });
 
@@ -39,14 +46,14 @@ export module NFT {
      * PURPOSE: Deletes a single NFT rental listing
      * TODO: If using an internal wallet as a third party escrow, return the NFT. Wait until rental ends if rented.
      */
-    router.delete('/listing', async (req: Request, res: Response) => {
-        
+    router.delete('/listing', verifySignature, async (req: Request, res: Response) => {
+
     });
 
     /**
      * PURPOSE: Create a rental object for a corresponding NFT rental listing and accept payment
      */
-    router.post('rent', async (req: Request, res: Response) => {
+    router.post('rent', verifySignature, async (req: Request, res: Response) => {
         
     });
 }
