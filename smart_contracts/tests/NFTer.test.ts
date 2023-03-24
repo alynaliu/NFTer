72115f678d59697c81fd312044ea1efe09770172
Edward <edwardchang2000@gmail.com>
2023-03-23 22:14:24 -0400

    Switch from web3 to ethers

--- smart_contracts/tests/NFTer.test.ts
+++ smart_contracts/tests/NFTer.test.ts
@@ -1,21 +1,19 @@
 // @ts-nocheck
 import dotenv from 'dotenv'
-import Accounts from 'web3-eth-accounts'
-import Web3EthContract from 'web3-eth-contract'
-import Web3WsProvider from 'web3-providers-ws'
+import { ethers } from 'hardhat'
 
+import IERC4907 from '../artifacts/contracts/IERC4907.sol/IERC4907.json'
 import NFTer from '../artifacts/contracts/NFTer.sol/NFTer.json'
 
 dotenv.config();
 
-const provider = new Web3WsProvider(`wss://${process.env.TEST_WEB3_PROVIDER}`);
-const accounts = new Accounts(provider);
-const account = accounts.privateKeyToAccount(process.env.PRIVATE_KEY);
-
-Web3EthContract.setProvider(provider);
-const contract = new Web3EthContract(NFTer.abi, process.env.NFTER_CONTRACT, {
-    from: account.address
-});
+const alchemy = new ethers.providers.AlchemyProvider(
+    'maticmum',
+    process.env.ALCHEMY_API_KEY
+);
+const account = new ethers.Wallet(process.env.PRIVATE_KEY as string, alchemy);
+const contract = new ethers.Contract(process.env.NFTER_CONTRACT, NFTer.abi, account);
+const NFTcontract = new ethers.Contract(process.env.TESTNFT_CONTRACT, IERC4907.abi, account);
 
 //First create a test NFTer and TestNFT contract with the commands in the readme.
 
@@ -23,8 +21,27 @@ const contract = new Web3EthContract(NFTer.abi, process.env.NFTER_CONTRACT, {
 /* To test: go to OpenSea, send the NFT to test contract. 
 Go to mumbai.polygonscan.com to see if a child contract is made and if the NFT was moved to that escrow.*/
 
-main();
+function testIERC4907() {
+    NFTcontract.on('UpdateUser',async (tokenId, user, expires) => {
+        console.log(tokenId, user, expires);
+    });
+    console.log(await NFTcontract.userOf(1));
+    console.log(await NFTcontract.userExpires(1));
+    await NFTcontract.setUser(1, "0x57d988e4C48fC2ed50fd13BDde47F677d0812bA7", 1679690000);
+    console.log(await NFTcontract.userOf(1));
+    console.log(await NFTcontract.userExpires(1));
+}
+
 async function main() {
     //Verify that the smart contract is working
-    console.log(await contract.methods.getTime().call());
+    console.log(await contract.methods.getTime());
+
+    testIERC4907
 }
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
