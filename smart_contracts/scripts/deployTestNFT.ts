7e97e9efd150c5d3c5185f28d22028ef9d9620a6
Edward <edwardchang2000@gmail.com>
2023-03-23 21:13:00 -0400

    Update deployTestNFT.ts

--- smart_contracts/scripts/deployTestNFT.ts
+++ smart_contracts/scripts/deployTestNFT.ts
@@ -15,24 +15,33 @@ async function main() {
   console.log("Contract deployed to: ", contract.address);
   console.log("Contract deployed by (Owner): ", owner.address, "\n");
 
-  const amountToMint = 100;
-  for(let i=0; i<amountToMint; i++) {
-    const tokenId = await contract.getCurrentTokenID();
-    const imageId = tokenId % 10;
+  //On mint
+  contract.on('Transfer', async (from, to, tokenId) => {
+    console.log(`NFT ${tokenId} Minted`);
 
+    const imageId = tokenId.toNumber() % 10;
     const content = await fs.promises.readFile(`./images/${imageId}.jpg`);
     const type = mime.getType(`./images/${imageId}.jpg`) as string;
     const image = new File([content], imageId.toString(), { type });
+
     const result = await nftstorage.store({
       "name": `TEST NFT ${tokenId}`,
       "description": "This is a really cool NFT",
       "image": image
     });
-    
-    const txn = await contract.mintNFT(process.env.USER_PUBLIC_KEY, result.url);
+    const txn = await contract.setMetadata(tokenId, result.url);
     await txn.wait();
-    console.log(`NFT "${tokenId}" minted successfully!`);
+    console.log(`Set metadata for NFT ${tokenId}`);
+  });
+
+  const amountToMint = 1;
+  for(let i=0; i<amountToMint; i++) {
+    const txn = await contract.mintNFT(process.env.USER_PUBLIC_KEY);
+    await txn.wait();
   }
+
+  console.log("Finished minting. Sleeping for a minute");
+  await new Promise(r => setTimeout(r, 60000));
 }
 
 main()
